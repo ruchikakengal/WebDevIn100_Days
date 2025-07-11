9eaaa65164e0d71016b7b1acc4e3c55c907e73c9
Abhirup-261004 <abhirupn2004@gmail.com>
2025-07-11 23:12:28 +0530

    Merge Conflict Resolved

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function fillTable() {
         ["Day 2", "Digital Clock", "./public/digital_clock/digitalclock.html"],
         ["Day 3", "ASCII Art Generator (by Amaan Syed)", "./public/AsciiArtGenerator/index.html",],
         ["Day 4", "Physics Simulation (by Vishisht Dwivedi)", "./public/physics_simulation/index.html"],
-        ["Day 5", "Quote Generator (by Sania Rajput)", "./public/QuoteGenerator/index.html"],
+        ["Day 5","Quote Generator (by Sania Rajput)", "./public/QuoteGenerator/index.html"],
         ["Day 6", " ",],
         ["Day 7", " ",],
         ["Day 8", "Car Simulation (by Abhirup Nandi)","https://abhirup-261004.github.io/Car-Animation/"],
