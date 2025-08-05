6c0dfd3ad47b02d768c7307b182a45e9611ec847
Pavanmali0281 <pavanmali0281@gmail.com>
2025-08-05 14:07:51 +0530

    Update index.js - fix TypeRush path reference

--- index.js
+++ index.js
@@ -139,9 +139,10 @@ function fillTable() {
           ["Day 35", "Book Recommendation", "https://book-recomendation.netlify.app/"],
           ["Day 36", "World Clock", "public/World_Clock/index.html"],
           ["Day36", "World Clock", "public/World_Clock/index.html"],
+        ["Day 37", "TypeRush", "./public/TypeRush/index.html"],
           ["Day 38", "Gradient Generator", "public/Gradient_Generator/index.html"],
           ["Day 40", "Space War Game", "./public/Space-War-Game/index.html"],
-           ["Day 41", "TypeMaster", "./public/TypeMaster/index.html"],
+       
           ["Day 42", "Student Grade Analyzer", "./public/Student_Grade_Analyzer/index.html"],
           ["Day 44", "Notes App", "./public/Day-36_Notes_App/index.html"],
           ["Day 46", "Mood Based Music Suggester", "./public/Mood_Music_Suggester/index.html"],
