e3e0f1748d4be1c2e4fd5589ff6ef808f2cbfeb4
SV2111004 <gp.shubhra78@gmail.com>
2025-08-10 18:18:54 +0530

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -56,17 +56,6 @@ class WebDev100Days {
     if (page && page !== this.currentPage) {
       this.currentPage = page;
       this.renderTable();
-
-      // Scroll headings se start ho
-      setTimeout(() => {
-        const tableHead = document.querySelector("table thead");
-        if (tableHead) {
-          // Agar koi fixed header ya navbar height hai to uska offset nikal lo
-          const headerOffset = 80; // yahan apne header ki actual height set karo
-          const y = tableHead.getBoundingClientRect().top + window.scrollY - headerOffset;
-          window.scrollTo({ top: y, behavior: "smooth" });
-        }
-      }, 50);
     }
   }
 });
