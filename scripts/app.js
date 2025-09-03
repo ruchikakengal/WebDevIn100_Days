848c9337e63390330f9cfc6fbd78f4589ed39cf8
ParikhShreya <shreyaparikh2006@gmail.com>
2025-09-03 21:57:42 +0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -579,7 +579,7 @@ class WebDev100Days {
                 originalDay: 46,
                 name: "Attendance Tracker Website",
                 description: "Attendance Tracking Website For Students",
-                demoLink: "attendance_tracker.html",
+                demoLink: "./public/Attendace_Tracker/attendance_tracker",
                 category: "education",
                 technologies: ["HTML", "CSS", "JavaScript"],
                 features: ["Student Attendance Tracking", "Add-remove students", "Present/Absent"]
